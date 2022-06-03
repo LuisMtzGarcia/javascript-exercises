2d1729e262af0d0d34436f698d071a68a94d142e
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-06-03 16:33:56 -0500

    Finish exercise 05 Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let top;
+    let bottom;
 
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number' || 
+        num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    if (num1 > num2) {
+        top = num1;
+        bottom = num2;
+    } else {
+        top = num2;
+        bottom = num1;
+    }
+
+    for (let i = bottom; i <= top; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
