1a4d581be04a74d32cbb44c94fde2cba150cbdba
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-06-06 14:27:03 -0500

    Finish exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    let result = 0;
+    if (num < 0) {
+        return "OOPS";
+    } else if (num <= 1) {
+        return num;
+    } else {
+        result = fibonacci(num-1) + fibonacci(num-2);
+        return result;
+    }
+
 
 };
 
