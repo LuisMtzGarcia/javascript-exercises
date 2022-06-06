a6cc45b99f3dc590338262e7b2006d045631ebb5
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-06-06 14:06:19 -0500

    Finish exercise 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let reversedString = '';
+    let cleanString = '';
 
+    for (let i = (string.length - 1); i >= 0; i--) {
+        if (isLetter(string[i])) {
+            reversedString += string[i];
+        }
+    }
+
+    for (let i = 0; i < string.length; i++) {
+        if (isLetter(string[i])) {
+            cleanString += string[i];
+        }
+    }
+
+    return cleanString == reversedString;
 };
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
