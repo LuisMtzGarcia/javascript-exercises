fd939323a68a77de70fcdcfe4759c3d806391114
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-06-06 13:51:13 -0500

    Finish exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 0;
+  
+  if (result === 0 && array.length > 0) {
+    result = 1;
+  }
 
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let result = num;
+	for (let i = 1; i < power; i++) {
+    result *= num;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
