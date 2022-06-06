38b734d467dbc9a9540f2b7e6f93d7ced83da1c0
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-06-06 16:34:13 -0500

    Finish exercise 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    currentYear = parseInt(currentYear);
 
+    let oldest = people[0];
+
+    let ages = [];
+
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath) {
+            let age = parseInt(people[i].yearOfDeath) - parseInt(people[i].yearOfBirth);
+            ages.push(age);
+        } else {
+            let age = currentYear - parseInt(people[i].yearOfBirth);
+            ages.push(age);
+        }
+    }
+
+    let max = 0;
+    for (let i = 0; i < ages.length; i++) {
+        if (ages[i] > max) {
+            max = ages[i];
+            oldest = people[i];
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
